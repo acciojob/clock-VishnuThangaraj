8e4421bdab6c1a565d1cf19d08ee72580c3ba61c
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-29 22:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const displayArea = document.getElementById('timer');
+
+function clock(){
+	const date = new Date();
+	displayArea.innerHTML = date.toString();
+}
+
+setInterval(clock, 1000);
